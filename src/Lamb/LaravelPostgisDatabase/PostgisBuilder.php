50c4728bec03e3b9c9171b37c5d41931408552ab
gxela <alex@goretoy.com>
2014-04-21 00:14:00 -0700

    Updated PostgisBuilder \Closure instead of Closure

--- src/Lamb/LaravelPostgisDatabase/PostgisBuilder.php
+++ src/Lamb/LaravelPostgisDatabase/PostgisBuilder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Schema\Builder;
 
 class PostgisBuilder extends Builder {
 
-	protected function createBlueprint($table, Closure $callback = null)
+	protected function createBlueprint($table, \Closure $callback = null)
 	{
 		return new PostgisBlueprint($table, $callback);
 	}
